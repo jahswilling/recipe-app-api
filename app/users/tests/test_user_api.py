27c3fbf95aae3cc60c738145338b571cc0f097a2
Root Code <jahswilling@gmail.com>
2020-07-15 01:46:07 +0100

    create end point tags

--- app/users/tests/test_user_api.py
+++ app/users/tests/test_user_api.py
@@ -128,7 +128,7 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        
 
     def test_create_token_missing_field(self):
         """Test that email and password are required"""
